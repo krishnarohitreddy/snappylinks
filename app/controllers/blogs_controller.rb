1581a51821e32e7a3f5b6aa2ac1ff1b76bf86e5b
rohit <krishnarohitreddy@gmail.com>
2014-09-29 12:02:46 -0500

    second iteration

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,24 @@
+class BlogsController < ApplicationController
+	def create
+		@blogs = Blog.new(blog_params)
+		@blogs.save
+		render 'student'
+	end
+	def blog_params
+		params.require(:blogs).permit(:name,:email,:message)
+	end
+	def index
+	end
+	def show
+	end
+	def student
+	end
+	def ruby
+	end
+	def rails
+	end
+	def c
+	end
+	def java
+	end
+end
---
+++ db/migrate/20140918001629_create_blogs.rb
@@ -0,0 +1,11 @@
+class CreateBlogs < ActiveRecord::Migration
+  def change
+    create_table :blogs do |t|
+    	t.string :name
+    	t.string :email
+    	t.string :message
+
+      t.timestamps
+    end
+  end
+end

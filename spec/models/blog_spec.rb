98a313695f97e016062f173c1bb85f912c562332
Rohit krishna <krishnarohitreddy@gmail.com>
2014-11-16 00:25:57 -0600

    included rspec

---
+++ spec/models/blog_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe Blog do
+  let(:blog) { Blog.create(:name => 'rohit', :email => 'krishnarod@gmail.com', :message => 'this is a message') }
+  subject { blog }
+
+  context "validations" do
+    it "should validate presence of name" do
+      expect(subject.name).to eq('rohit')
+    end
+
+    it "should validate presence of email" do
+      expect(subject.email).to eq('krishnarod@gmail.com')
+    end
+  end
+end
